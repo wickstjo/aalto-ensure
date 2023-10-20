be0d8376ce8b5f524821ded4ac50888fa161130a
wickstjo <jfwick@gmail.com>
2023-10-20 13:15:30 +0300

    Added some working monitoring configs

--- install/04-worker.sh
+++ install/04-worker.sh
@@ -7,20 +7,21 @@
 sudo swapoff -a
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
+# sudo swapoff -a && systemctl stop firewalld && sudo systemctl disable firewalld
 
 echo -e "\n##########################################################################################\n"
 
-
-# sudo swapoff -a && systemctl stop firewalld && sudo systemctl disable firewalld
-
-kubeadm join 192.168.1.120:6443 --token lxl5v6.aldzthzvq3861w95 --discovery-token-ca-cert-hash sha256:f35a08d8302f4a0e9c4e1e1fd502a8e2661d2038d8fb0dcbf404be491b88089f --cri-socket=unix:///var/run/cri-dockerd.sock
+kubeadm join 192.168.1.120:6443 \
+    --token lxl5v6.aldzthzvq3861w95 \
+    --discovery-token-ca-cert-hash sha256:f35a08d8302f4a0e9c4e1e1fd502a8e2661d2038d8fb0dcbf404be491b88089f \
+    --cri-socket=unix:///var/run/cri-dockerd.sock
 
 # --cri-socket=unix:///var/run/cri-dockerd.sock
 
-# kubeadm join 192.168.1.7:6443 \
-#     --token vddjj0.j7mxwxbym1plzo5d \
-#     --discovery-token-ca-cert-hash sha256:5c8a41fec2cebd2ecae8c9447e02cd426ae3f199c4fa9872a6216ecfa7ffe904 \
-#     --cri-socket=unix:///var/run/cri-dockerd.sock
+
+
+
+
 
 # clear && kubectl get nodes --watch
 # kubectl label nodes worker2 kubernetes.io/role=worker
