c2e31001d6e549f20cecdc49e85ff864d15082f7
wickstjo <jfwick@gmail.com>
2023-12-04 16:12:18 +0200

    Added some small tweaks in an attempt to stabilize experiments

--- yolo/app/feeder.py
+++ yolo/app/feeder.py
@@ -1,10 +1,8 @@
-from utilz.dataset_utils import parse_dataset
-from utilz.misc import resource_exists, log, create_lock
+from utilz.dataset_utils import load_dataset
+from utilz.misc import resource_exists, log, create_lock, resize_array
 from utilz.kafka_utils import create_producer
-from multiprocessing import Queue
-from queue import Empty
-from threading import Thread
-import time
+from threading import Thread, Semaphore
+import time, math, random
 
 def run():
 
@@ -17,10 +15,14 @@ def run():
             'fps': 5,
             'repeat': 1,
         },
-        'thread_pool': 3,
-        'queue_size': 1000,
-        'queue_delay': 2,
-        'frame_cooldown': 0.1,
+        'num_threads': 4,
+
+        # EXPERIMENT DETAILS
+        'experiment': {
+            'max_mbs': 4,
+            'n_breakpoints': 400,
+            'duration': (60*60*10), 
+        }
     }
 
     ########################################################################################
@@ -30,13 +32,16 @@ def run():
     if not resource_exists(f'./datasets/{args["dataset"]["name"]}.hdf5'):
         return
     
-    # INSTANTIATE THREAD PARAMS
+    # INSTANTIATE THREAD LOCKS
     thread_lock = create_lock()
+    semaphore = Semaphore(1)
+
+    # KEEP TRACK THREADS AND KAFKA PRODUCERS
     threads = []
     kafka_producers = []
 
     # CREATE KAFKA PRODUCERS FOR EACH THREAD
-    for _ in range(args['thread_pool']):
+    for _ in range(args['num_threads']):
         kafka_producer = create_producer()
         kafka_producers.append(kafka_producer)
 
@@ -44,43 +49,127 @@ def run():
     if not kafka_producers[0].connected():
         return
 
-    # START GRADUALLY LOADING DATASET INTO A QUEUE BUFFER
-    queue = Queue(maxsize=args['queue_size'])
+    # LOAD THE DATASET
+    dataset = load_dataset(args['dataset'])
+    dataset_length = len(dataset)
 
-    # START PARSING DATASET INTO BUFFER -- IN ANOTHER THREAD
-    thread = Thread(target=parse_dataset, args=(args['dataset'], queue, thread_lock))
-    threads.append(thread)
-    thread.start()
+    ########################################################################################
+    ########################################################################################
 
-    # WAIT FOR THQ BUFFER TO FILL ABIT
-    log(f'LOADING BUFFER FOR {args["queue_delay"]} SECONDS')
-    time.sleep(args['queue_delay'])
+    def experiment_handler(lock):
+        global action_cooldown
+
+        # THE DEFAULT DAYNIGHT CYCLE WORKLOAD PERCENTAGES (01 => 23)
+        default_cycle = [
+            0.24, 0.28, 0.32, 0.36, 0.40, 0.53, 
+            0.67, 0.80, 0.73, 0.65, 0.58, 0.50, 
+            0.58, 0.66, 0.74, 0.82, 0.95, 0.90, 
+            0.85, 0.80, 0.65, 0.50, 0.35, 0.20
+        ]
+
+        # SCALE THE ARRAY WHILE MAINTAINING RATIOS
+        real_cycle = resize_array(
+            default_cycle, 
+            args['experiment']['n_breakpoints']
+        )
+
+        # COMPUTE THE EQUAL TIME SLIVER
+        time_sliver = args['experiment']['duration'] / args['experiment']['n_breakpoints']
+
+        # COMPUTE THE BYTESIZE OF THE AVERAGE DATASET ITEM
+        avg_dataset_item_size = math.ceil(sum([len(x) for x in dataset]) / len(dataset))
+
+        log('STARTING EXPERIMENT WITH:')
+        log(f'N_BREAKPOINTS: ({args["experiment"]["n_breakpoints"]})')
+        log(f'MAX MB/s:  ({args["experiment"]["max_mbs"]})')
+        log(f'TOTAL DURATION: ({args["experiment"]["duration"]})')
+        log(f'SLIVER DURATION: ({time_sliver})')
+
+        # STAY ACTIVE UNTIL LOCK IS MANUALLY KILLED
+        while lock.is_active():
+
+            # NO MORE BREAKPOINTS LEFT: KILL ALL THE THREADS
+            if len(real_cycle) == 0:
+                log('LAST EXPERIMENT BREAKPOINT RAN, TERMINATING..')
+                lock.kill()
+                break
+
+            # OTHERWISE, FETCH THE NEXT INTERVAL
+            mbs_interval = real_cycle.pop(0) * args['experiment']['max_mbs']
+            log(f'SET NEW INPUT INTERVAL: ({time_sliver}s @ {mbs_interval} MB/s)')
+
+            # COMPUTE THE NEW ACTION COOLDOWN
+            events_per_second = (mbs_interval * 1000000) / avg_dataset_item_size
+            new_cooldown = (1 / (events_per_second / args['num_threads']))
+
+            # SAFELY SET THE NEXT COOLDOWN
+            with semaphore:
+                action_cooldown = new_cooldown
+
+            # ON THE FIRST RUN, BUSY WAIT TO SYNC THREADS
+            while time.time() < experiment_start:
+                pass
+            
+            # THEN SLEEP UNTIL THE NEXT BREAKPOINT
+            time.sleep(time_sliver)
+
+    ########################################################################################
+    ########################################################################################
 
     # PRODUCER THREAD WORK LOOP
     def thread_work(nth_thread, lock):
-        while queue._notempty and lock.is_active():
-            try:
-                # SELECT NEXT BUFFER ITEM
-                item = queue.get(block=True, timeout=5)
+        global action_cooldown
 
-                # PROCESS EACH FRAME'S IMG MATRIX
-                for _, frame in item.data.items():
-                    if lock.is_active():
+        # RANDOMLY PICK A STARTING INDEX FROM THE DATASET
+        next_index = random.randrange(dataset_length)
+        cooldown = None
 
-                        # PUSH IT TO KAFKA
-                        kafka_producers[nth_thread - 1].push_msg('yolo_input', frame.data.tobytes())
-                        time.sleep(args['frame_cooldown'])
+        # BUSY WAIT FOR ABIT TO SYNC THREADS
+        while time.time() < experiment_start:
+            pass
 
-            # DIE PEACEFULLY WHEN QUEUE IS EMPTY
-            except Empty:
-                log(f'QUEUE WAS EMPTY, THREAD {nth_thread} DYING..')
-                break
+        log(f'THREAD {nth_thread} HAS STARTED FROM INDEX {next_index}')
 
-    # CREATE & START WORKER THREADS
+        # KEEP GOING UNTIL LOCK IS MANUALLY
+        while lock.is_active():
+            started = time.time()
+
+            # SELECT NEXT BUFFER ITEM
+            item = dataset[next_index]
+            kafka_producers[nth_thread - 1].push_msg('yolo_input', item.tobytes())
+            
+            # FETCH THE LATEST ACTION COOLDOWN
+            with semaphore:
+                cooldown = action_cooldown
+
+            # COMPUTE THE ADJUSTED ACTION COOLDOWN, THEN TAKE A NAP
+            ended = time.time()
+            action_duration = ended - started
+            adjusted_cooldown = max(cooldown - action_duration, 0)
+            time.sleep(adjusted_cooldown)
+
+            # INCREMENT ROLLING INDEX
+            next_index = (next_index+1) % dataset_length
+
+    ########################################################################################
+    ########################################################################################
+
     try:
-        log(f'STARTING PRODUCER THREAD POOL ({args["thread_pool"]})')
+    
+        # SHARED ACTION COOLDOWN FOR WORKER THREADS
+        # TIMESTAMP FOR THREADS TO SYNC TO
+        action_cooldown = None
+        experiment_start = time.time() + 3
 
-        for nth in range(args['thread_pool']):
+        # CREATE THE EXPERIMENT HANDLER
+        log(f'CREATING EXPERIMENT HANDLER')
+        handler_thread = Thread(target=experiment_handler, args=(thread_lock,))
+        threads.append(handler_thread)
+        handler_thread.start()
+
+        log(f'CREATING PRODUCER THREAD POOL ({args["num_threads"]})')
+
+        for nth in range(args['num_threads']):
             thread = Thread(target=thread_work, args=(nth+1, thread_lock))
             threads.append(thread)
             thread.start()
@@ -91,7 +180,6 @@ def run():
     # TERMINATE MAIN PROCESS AND KILL HELPER THREADS
     except KeyboardInterrupt:
         thread_lock.kill()
-        queue.cancel_join_thread()
         log('WORKER & THREADS MANUALLY KILLED..', True)
 
 run()
--- yolo/app/kafka_init.py
+++ yolo/app/kafka_init.py
@@ -2,6 +2,10 @@ from confluent_kafka.admin import AdminClient, NewPartitions, NewTopic
 from confluent_kafka import TopicPartition
 import json, time
 
+from utilz.kafka_utils import create_producer, create_consumer
+from utilz.misc import create_lock
+from threading import Thread
+
 admin_client = AdminClient({
     'bootstrap.servers': ','.join(['130.233.193.117:' + str(x) for x in [10001, 10002, 10003]]),
 })
@@ -37,6 +41,9 @@ def create_topic(name, partitions, replication):
     
     return True
 
+###########################################################################################
+###########################################################################################
+
 try:
     # CREATE YOLO INPUT TOPIC WITH 34 PARTITIONS
     create_topic(
@@ -52,11 +59,36 @@ try:
         replication=1
     )
 
-    print('CREATING TOPICS...')
-
 except Exception as err:
     print(err)
 
 # PRINT CURRENT KAFKA TOPIC STATE
 time.sleep(1)
 print(json.dumps(query_topics(), indent=4))
+
+###################################################################################
+###################################################################################
+
+# PRODUCE AND CONSUME ONE MESSAGE FROM THE INPUT TOPIC
+
+def test_topic(topic_name):
+    thread_lock = create_lock()
+
+    def cons(lock):
+        kafka_client = create_consumer(topic_name)
+
+        while lock.is_active():
+            kafka_client.poll_next(1, lock, lambda x, y: lock.kill())
+
+    consumer_thread = Thread(target=cons, args=(thread_lock,))
+    consumer_thread.start()
+
+    time.sleep(2)
+
+    kafka_client = create_producer()
+    kafka_client.push_msg(topic_name, b'foo')
+
+    consumer_thread.join()
+
+test_topic('yolo_input')
+test_topic('yolo_output')
